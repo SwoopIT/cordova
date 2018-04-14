4532676349637c19d8bb70dd4270468dfc6ffb05
Unicorn <unicorncalcs@gmail.com>
2018-04-13 17:55:55 -1000

    bam

--- platforms/android/app/src/main/assets/www/js/main.js
+++ platforms/android/app/src/main/assets/www/js/main.js
@@ -8,8 +8,6 @@ var storeNames = {
 	bk: 'Burger King',
 	longs: 'Longs Drugs'
 };
-var thing = 'bk'
-console.log(storeNames[thing]);
 // Cache
 var storesDB, itemsDB, categoriesDB;
 
@@ -20,6 +18,9 @@ $(document).ready(function () {
 		console.log(data.currentTarget.id);
 		stores(data.currentTarget.id)
 	});
+	$('#navbar').hide();
+	$(document.body).addClass('blue');
+	$(document.body).removeClass('grey lighten-4');
 	cache();
 });
 
@@ -28,6 +29,9 @@ function sideClose() {
 }
 
 function stores(id) {
+	$('#navbar').show();
+	$(document.body).removeClass('blue');
+	$(document.body).addClass('grey lighten-4');
 	if (!id) {
 		$('#container').html(storesHTML);
 		$('#header').html('SwoopIt').addClass('blue-text').removeClass('black-text');
@@ -75,6 +79,9 @@ function loadItems(categoryName, storeId) {
 }
 
 function cart() {
+	$('#navbar').show();
+	$(document.body).removeClass('blue');
+	$(document.body).addClass('grey lighten-4');
 	$('#container').html(cartHTML);
 	$('#button').remove();
 	$('#header').html('Cart').addClass('black-text').removeClass('blue-text');
@@ -82,19 +89,21 @@ function cart() {
 		'            <li>\n' +
 		'                <a href="#" onclick="payment()"\n' +
 		'                   class="blue-text"><i\n' +
-		'                        class="material-icons">storeping_basket</i></a>\n' +
+		'                        class="material-icons">shopping_basket</i></a>\n' +
 		'            </li>\n' +
 		'        </ul>');
 	$('#menu').html('<a href="#" data-target="slide-out"\n' +
 		'                   class="sidenav-trigger blue-text"><i\n' +
 		'                        class="material-icons">menu</i></a>');
-	$('.dropdown-trigger').dropdown();
 
 	sideClose();
 }
 
 
 function orders() {
+	$('#navbar').show();
+	$(document.body).removeClass('blue');
+	$(document.body).addClass('grey lighten-4');
 	$('#container').html(ordersHTML);
 	$('#button').remove();
 	$('#header').html('Orders').addClass('black-text').removeClass('blue-text');
@@ -109,6 +118,9 @@ function orders() {
 }
 
 function settings() {
+	$('#navbar').show();
+	$(document.body).removeClass('blue');
+	$(document.body).addClass('grey lighten-4');
 	$('#container').html(settingsHTML);
 	$('#header').html('Settings').addClass('black-text').removeClass('blue-text');
 	sideClose();
@@ -147,6 +159,44 @@ function confirmOrder() {
 		'                        class="material-icons">arrow_backwards</i></a>');
 }
 
+function search() {
+	$('#navbar').show();
+	$(document.body).removeClass('blue');
+	$(document.body).addClass('grey lighten-4');
+	$('#container').html(searchHTML);
+	$('#header').html('Search').addClass('black-text').removeClass('blue-text');
+	var auto = document.querySelector('.autocomplete');
+	var complete = M.Autocomplete.init(auto, {
+		data: {
+			"Arizona": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Fries-Small-Medium.png",
+			"Big Mac": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Fries-Small-Medium.png",
+			"Fries": "https://www.mcdonalds.com/is/image/content/dam/usa/nutrition/items/regular/desktop/t-mcdonalds-Fries-Small-Medium.png"
+		},
+		onAutocomplete: function () {
+			$('#search-form').submit();
+		}
+	});
+	sideClose()
+}
+
+function searchItems(query) {
+	$('#search-coll').html('');
+	for (var i = 0; i < itemsDB.length; i++) {
+		if (itemsDB[i].name.toLowerCase().indexOf(query.toLowerCase()) >= 0) {
+			$('#search-coll').append('<a class="collection-item avatar black-text nohover" id="' + itemsDB[i].id + '">' +
+				'               <img src="' + itemsDB[i].img + '" alt="' + itemsDB[i].name + '" style="margin-top:10px" class="circle">' +
+				'                <span class="title black-text" style="font-weight: bold;">' + itemsDB[i].name + '</span>' +
+				'            <p>' +
+				'               <text>$' + itemsDB[i].price + '</text>' +
+				'                   <text ><i style="margin-top: -10px; border-radius: 50%" class="material-icons right blue-text hover" id="remove-' + itemsDB[i].id + '" onclick="selectFood(this, false)">remove</i></text>' +
+				'  <text ><i style="margin-top: -10px; border-radius: 50%" class="material-icons right blue-text hover" id="add-' + itemsDB[i].id + '" onclick="selectFood(this, true)">add</i></text>' +
+				'                  <br>' +
+				'                 <text style="font-weight: lighter;">' + 'Put something here' + '</text><text class="right blue-text" data-item-count="0" id="' + itemsDB[i].id + '-quantity" style="position: relative;transform: translateX(5px);">Quantity: 0</text>' +
+				'              </p>' +
+				'          </a>')
+		}
+	}
+}
 
 function selectFood(element) {
 	var parent = $(element).closest("a");
@@ -170,7 +220,7 @@ function selectFood(element) {
 
 
 var storePageHTML = '<div class="row">\n' +
-	'        <form onsubmit="searchStores()" class="col s12" style="height: 76px;">\n' +
+	'        <form onsubmit="searchStoreItems(); return false" class="col s12" style="height: 76px;">\n' +
 	'            <div class="row">\n' +
 	'                <div class="input-field col s12">\n' +
 	'                    <i class="material-icons prefix blue-text">search</i>\n' +
@@ -234,14 +284,14 @@ var settingsHTML = '<div class="collection black-text" style="margin-top: -5px">
 
 
 var cartHTML = '<div class="collection black-text" style="margin-top: -5px">\n' +
-	'        <a href="#" data-target="cart-drop" class="collection-item black-text dropdown-trigger">Mac Big<i class="material-icons right">arrow_drop_down</i></a>\n' +
-	'        <a href="#" data-target="cart-drop2" class="collection-item black-text dropdown-trigger">Fireworks - Blue Fire<i class="material-icons right">arrow_drop_down</i></a>\n' +
-	'        <a href="#" data-target="cart-drop3" class="collection-item black-text dropdown-trigger">Mint Oreo\'s<i class="material-icons right">arrow_drop_down</i></a>\n' +
-	'        <a href="#" data-target="cart-drop4" class="collection-item black-text dropdown-trigger">Gordon Ramsey\'s Rat Poison<i class="material-icons right">arrow_drop_down</i></a>\n' +
-	'    </div>';
+'        <a href="#" onclick="dropdown(this)" class="collection-item black-text">Today<i class="material-icons right">arrow_drop_down</i></a>\n' +
+'        <a href="#" class="collection-item black-text">2 Hours Ago<i class="material-icons right">arrow_drop_down</i></a>\n' +
+'        <a href="#" class="collection-item black-text">11/11/11<i class="material-icons right">arrow_drop_down</i></a>\n' +
+'        <a href="#" class="collection-item black-text">01/33/07<i class="material-icons right">arrow_drop_down</i></a>\n' +
+'    </div>';
 
 var categoryHTML = '<div class="row">\n' +
-	'        <form style="height: 85px;" onsubmit="searchCateogries()" class="col s12">\n' +
+	'        <form style="height: 85px;" onsubmit="searchStoreItems(); return false" class="col s12">\n' +
 	'            <div class="row">\n' +
 	'                <div class="input-field col s12">\n' +
 	'                    <i class="material-icons prefix blue-text">search</i>\n' +
@@ -256,7 +306,7 @@ var categoryHTML = '<div class="row">\n' +
 
 
 var storesHTML = ' <div class="row">\n' +
-	'        <form onsubmit="searchStores()" class="col s12" style="height: 76px;">\n' +
+	'        <form onsubmit="searchStores(); return false" class="col s12" style="height: 76px;">\n' +
 	'            <div class="row">\n' +
 	'                <div class="input-field col s12">\n' +
 	'                    <i class="material-icons prefix blue-text">search</i>\n' +
@@ -270,19 +320,33 @@ var storesHTML = ' <div class="row">\n' +
 	'        <div class="center cont-store store-block" id="mcd" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/mcd.jpg)">\n' +
 	'            <h4 class="" style="z-index: 2; opacity: 1">McDonald\'s</h4>\n' +
 	'        </div>\n' +
-	'        <div class="center cont-store store-block" id="kta" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/kta.jpg)">\n' +
-	'            <h4 class="" style="z-index: 2; opacity: 1">KTA</h4>\n' +
-	'        </div>\n' +
 	'        <div class="center cont-store store-block" id="bk" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/burger.jpg)">\n' +
 	'            <h4 class="" style="z-index: 2; opacity: 1">Burger King</h4>\n' +
 	'        </div>\n' +
 	'        <div class="center cont-store store-block" id="fdl" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/food.jpg)">\n' +
 	'            <h4 class="" style="z-index: 2; opacity: 1">Foodland</h4>\n' +
 	'        </div>\n' +
-	'        <div class="center cont-store store-block" id="longs" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/longs.jpg)">\n' +
-	'            <h4 class="" style="z-index: 2; opacity: 1">Longs Drugs</h4>\n' +
+	'        <div class="center cont-store store-block" id="lnl" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/lnl.jpg)">\n' +
+	'            <h4 class="" style="z-index: 2; opacity: 1">L&L Barbecue</h4>\n' +
+	'        </div>\n' +
+	'        <div class="center cont-store store-block" id="dom" style="background: linear-gradient(rgba(20,20,20, .3),rgba(20,20,20, .3)), url(img/dom.jpg)">\n' +
+	'            <h4 class="" style="z-index: 2; opacity: 1">Dominoes</h4>\n' +
 	'        </div>\n' +
 	'    </div>';
+
+var searchHTML = '<div class="row">\n' +
+	'        <form id="search-form" onsubmit="searchItems($(\'#item-search\').val()); return false" class="col s12" style="height: 76px;">\n' +
+	'            <div class="row">\n' +
+	'                <div class="input-field col s12">\n' +
+	'                    <i class="material-icons prefix blue-text">search</i>\n' +
+	'                    <input id="item-search" type="text" class="autocomplete">\n' +
+	'                    <label for="item-search">Search</label>\n' +
+	'                </div>\n' +
+	'            </div>\n' +
+	'        </form>\n' +
+	'    </div>' +
+	'	<div class="collection black-text" id="search-coll" style="margin: -20px 0 0 0;">' +
+	'   </div>';
 
 
 function isAvailable() {
@@ -319,6 +383,9 @@ function login() {
 			console.log(user);
 			M.toast({html: 'User ' + user.displayName + ' has logged in.'});
 			$('#account').html('<i class="material-icons">person</i>' + user.displayName);
+			$('#navbar').show();
+			$(document.body).removeClass('blue');
+			$(document.body).addClass('grey lighten-4');
 			stores();
 		},
 		function (err) {
